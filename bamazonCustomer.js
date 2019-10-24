9139b9f71e7c41a198532e41a0509eecd92a99ff
cl33per <leeper08@gmail.com>
2019-10-23 20:58:38 -0400

    reading database after first question. Need to make code for checking/updating database for inventory

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -3,20 +3,63 @@ var inquirer = require('inquirer');
 
 var connection = mysql.createConnection({
     host: "localhost",
-  
+
     // Your port; if not 3306
     port: 3306,
-  
+
     // Your username
     user: "root",
-  
+
     // Your password
     password: "ADU6pyNJ",
     database: "bamazon"
-  });
-  
-  connection.connect(function(err) {
+});
+
+connection.connect(function (err) {
     if (err) throw err;
-    console.log("connected as id " + connection.threadId);
-    connection.end();
-  });
+    customerView();
+});
+
+function customerView() {
+    connection.query("SELECT * FROM products", function (err, res) {
+        if (err) throw err;
+        // Log all results of the SELECT statement
+        console.log(res);
+        idSearch();
+
+    });
+};
+
+function idSearch() {
+    inquirer.prompt([
+    {
+        name: 'id',
+        type: 'number',
+        message: 'Enter the ID:'
+    },
+    // {
+    //     name: 'stock_quantity',
+    //     type: 'number',
+    //     message: 'Enter the Quantity:'
+    // }
+    ]).then(function (answers) {
+        // console.log(answers.id);
+        // console.log(answers.stock_quantity);
+        connection.query("SELECT * FROM products WHERE ?", { id: answers.id }, function(err, res) {
+            if (err) throw err;
+            console.log(
+              "id: " +
+                res[0].id +
+                " || Product: " +
+                res[0].product_name +
+                " || Department: " +
+                res[0].department_name +
+                " || Price: $" +
+                res[0].price+
+                " || QTY: " +
+                res[0]. stock_quantity
+            );
+            connection.end();
+          });
+        });
+    };
