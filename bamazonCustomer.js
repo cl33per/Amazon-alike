aa9cc958b2d6010867ca63dc8e132e6095c48459
cl33per <leeper08@gmail.com>
2019-10-25 09:19:13 -0400

    customerView cleanup and ui adjustments.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -1,7 +1,14 @@
+//Used to connect to the sql database and queries
 var mysql = require('mysql');
+//Used to asking questions and take in input
 var inquirer = require('inquirer');
+//Packages used to for better UI on terminal  
 var cli = require('pixl-cli');
+const chalk = require('chalk');
+// Empty arry for product table view cli
+var productInfo = [];
 
+// Connection variable with settings -- change password localhost root if testing on different envirorment
 var connection = mysql.createConnection({
     host: "localhost",
 
@@ -18,36 +25,63 @@ var connection = mysql.createConnection({
 
 connection.connect(function (err) {
     if (err) throw err;
-    customerView();
+    startIventoryPortal()
 });
-var productInfo = [];
 
+function startIventoryPortal() {
+    inquirer
+        .prompt({
+            name: "action",
+            type: "list",
+            message: chalk.green("Welcome to the Amazon-alike Iventory Portal. \nChoose a option below:"),
+            choices: [
+                "View Current Inventory",
+                "Place an Order",
+                "Exit"
+            ]
+        }).then(function (answer) {
+            switch (answer.action) {
+                case "View Current Inventory":
+                    customerView();
+                    break;
+
+                case "Place an Order":
+                    idSearch();
+                    break;
+
+                case "Exit":
+                    connection.end();
+                    break;
+            }
+        });
+};
+//After connection is made the inventory is viewed in a tabel like format, then fires the idSearch function
 function customerView() {
     connection.query("SELECT * FROM products", function (err, res) {
         if (err) throw err;
-        // Log all results of the SELECT statement
-        for (var i = 0; i< res.length; i++){
+        // Log all results of the SELECT statement. By interating through this cleans the data for the cli.table package
+        for (var i = 0; i < res.length; i++) {
             // console.log(res[i].id);
             productInfo.push([
                 res[i].id,
                 res[i].product_name,
                 res[i].department_name,
-                "$"+res[i].price,
+                "$" + res[i].price,
                 res[i].stock_quantity,
             ]);
         };
+        // Defines the columns and rows of the table
+        var rows = [
+            ["ID", "Product", "Department", "Price", "Quantity"],
+            ...productInfo
+        ];
+        //Prints the inventory in a table format.
         cli.print(
-            cli.box("Welcome to amazon-alike!") + "\n"
+            cli.table(rows) + "\n"
         );
-
-            var rows = [
-                ["ID", "Product", "Department", "Price", "Quantity"],
-                ...productInfo
-            ];
-            cli.print(
-                cli.table(rows) + "\n"
-            );
-        idSearch();
+        productInfo = []
+        startIventoryPortal();
+        
     });
 };
 
@@ -59,82 +93,54 @@ function idSearch() {
                 if (isNaN(value) === false && parseInt(value) > 0 && parseInt(value) <= 10) {
                     return true;
                 }
+                console.log(chalk.red.bold(" ID Must be a number"));
                 return false;
             }
         },
         {
             name: 'stock_quantity',
-            type: 'number',
-            message: 'Enter the Quantity:'
+            message: 'Enter the Quantity:',
+            validate: function (value) {
+                if (isNaN(value) === false && parseInt(value) > 0 && parseInt(value) <= 10) {
+                    return true;
+                }
+                console.log(chalk.red.bold(" Quantity Must be a number"));
+                return false;
+            }
         }
     ]).then(function (answers) {
-        // console.log(answers.id);
-        // console.log(answers.stock_quantity);
         connection.query("SELECT * FROM products WHERE ?", {
             id: answers.id
         }, function (err, res) {
             if (err) throw err;
-            // console.log(
-            //     "id: " +
-            //     res[0].id +
-            //     " || Product: " +
-            //     res[0].product_name +
-            //     " || Department: " +
-            //     res[0].department_name +
-            //     " || Price: $" +
-            //     res[0].price +
-            //     " || QTY: " +
-            //     res[0].stock_quantity
-            //);
-            updateProduct(answers,res);
+            updateProduct(answers, res);
         });
     });
 };
-function updateProduct(answers,res) {
+
+function updateProduct(answers, res) {
     var oldStock = res[0].stock_quantity //10
     var newStock = oldStock - answers.stock_quantity; // 10 - 10 = 0
-     // console.log(productInfo);
-    if (newStock <= 0 ){
-        console.log("Insufficient quantity!");
-        connection.end();
-    }else{
-    connection.query(
-        "UPDATE products SET ? WHERE ?",
-        [{
-                stock_quantity: newStock
-            },
-            {
-                id: answers.id
+    // console.log(productInfo);
+    if (newStock < 0) {
+        console.log(chalk.red.bold("\n" + "Insufficient quantity!" + "\n"));
+        startIventoryPortal()
+    } else {
+        connection.query(
+            "UPDATE products SET ? WHERE ?",
+            [{
+                    stock_quantity: newStock
+                },
+                {
+                    id: answers.id
+                }
+            ],
+            function (err, res) {
+                if (err) throw err;
+                console.log(res.affectedRows + " product updated!\n");
+                productInfo = []
+                customerView();
             }
-        ],
-        function (err, res) {
-            if (err) throw err;
-            console.log(res.affectedRows + " product updated!\n");
-            for (var i = 0; i < res.length; i++) {
-                // console.log(res[i].id);
-                productInfo.push([
-                    res[i].id,
-                    res[i].product_name,
-                    res[i].department_name,
-                    "$" + res[i].price,
-                    res[i].stock_quantity,
-                ]);
-            };
-            cli.print(
-                cli.box("Welcome to amazon-alike!") + "\n"
-            );
-
-            var rows = [
-                ["ID", "Product", "Department", "Price", "Quantity"],
-                ...productInfo
-            ];
-            cli.print(
-                cli.table(rows) + "\n"
-            );
-            // console.log(newStock)
-            connection.end();
-            // Call deleteProduct AFTER the UPDATE completes
-        }
-    );
-};
+        );
+    };
 };
